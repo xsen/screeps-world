4fa0a538311c9138accee99cfd37caa0bc62caa1
xsen <evgeny.leshchenko@gmail.com>
2025-03-18 13:29:08 +0500

    save statistic in memory

--- src/main.ts
+++ src/main.ts
@@ -18,4 +18,47 @@ export const loop = () => {
     spawner.execute(data);
     planner.execute(data);
   });
+
+  if (Game.time % 10 === 0) {
+    updateStats();
+  }
 };
+
+function updateStats() {
+  const stats: GameStats = {
+    gcl: {
+      progress: Game.gcl.progress,
+      progressTotal: Game.gcl.progressTotal,
+      level: Game.gcl.level,
+    },
+    cpu: {
+      bucket: Game.cpu.bucket,
+      limit: Game.cpu.limit,
+      used: Game.cpu.getUsed(),
+    },
+    rooms: {},
+  };
+
+  for (const roomName of Object.keys(Game.rooms)) {
+    const room = Game.rooms[roomName];
+    if (room.controller?.my == false) continue;
+
+    stats.rooms[roomName] = {
+      energy: room.energyAvailable,
+      minerals: {},
+      controllerProgress: room.controller ? room.controller.progress : 0,
+      creeps: Object.keys(Game.creeps).filter(
+        (creep) => Game.creeps[creep].room.name === roomName,
+      ).length,
+      storageEnergy: room.storage ? room.storage.store.energy : 0,
+      terminalEnergy: room.terminal ? room.terminal.store.energy : 0,
+      energyAvailable: room.energyAvailable,
+      energyCapacityAvailable: room.energyCapacityAvailable,
+      controllerProgressTotal: room.controller?.progressTotal,
+      controllerLevel: room.controller?.level,
+    };
+  }
+
+  RawMemory.setActiveSegments([0]);
+  RawMemory.segments[0] = JSON.stringify(stats);
+}
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,44 @@
 declare global {
-  var myFunction: (arg?: string) => number;
-
   interface Memory {
     log: any;
+  }
+
+  interface RoomStats {
+    energy: number;
+    minerals: { [type: string]: number };
+    controllerProgress?: number;
+    creeps: number;
+    storageEnergy: number;
+    terminalEnergy: number;
+    energyAvailable: number;
+    energyCapacityAvailable: number;
+    controllerProgressTotal?: number;
+    controllerLevel?: number;
+  }
+
+  interface GCLStats {
+    progress: number;
+    progressTotal: number;
+    level: number;
+  }
+
+  interface CPUStats {
+    bucket: number;
+    limit: number;
+    used: number;
+  }
+
+  interface GameStats {
+    gcl: GCLStats;
+    rooms: { [roomName: string]: RoomStats };
+    cpu: CPUStats;
+  }
+
+  interface Stats {
+    gcl: any;
+    cpu: any;
+    rooms: any;
+    time: number;
   }
 
   interface Creep {
